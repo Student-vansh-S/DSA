4c4820fb8e7c926f5e7143da1a32adc62e51d54c
Vansh <sainivansh8116@gmail.com>
2026-01-29 23:09:50 +0530

    Sort Array Of 0,1,2

---
+++ Array/SortArrayOf012.java
@@ -0,0 +1,38 @@
+public class SortArrayOf012 {
+    public static void sortColor(int[] nums){
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else { // nums[mid] == 2
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void print(int[] nums){
+        for(int i:nums){
+            System.out.print(i+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColor(nums);
+        print(nums);
+    }
+    
+}
