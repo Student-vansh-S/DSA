0d6c1c3c0bbb4e0b7736516265cea50b245dc299
Vansh <sainivansh8116@gmail.com>
2026-02-04 23:22:34 +0530

    Merge Overlapping Intervals

---
+++ 2D Array/MergeIntervals.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        //Sort
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
+        //Merge 
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
+            }
+        }
+        result.add(current);
+        return result.toArray(new int[result.size()][]);
+    }
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] ans = merge(intervals);
+        for (int[] a : ans) {
+            System.out.print(Arrays.toString(a)+",");
+        }
+    }
+}
