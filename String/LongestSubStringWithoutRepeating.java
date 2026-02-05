dd6ffa559fda91cb4a8aa0eddfa697bacebae445
Vansh <sainivansh8116@gmail.com>
2026-02-06 00:40:22 +0530

    Longest Sub String Without Repeating

--- String/LongestSubStringWithoutRepeating.java
+++ String/LongestSubStringWithoutRepeating.java
@@ -1,17 +1,32 @@
 import java.util.*;
 public class LongestSubStringWithoutRepeating {
     public static int longestSubString(String s){
-        Map<Character, Integer> last = new HashMap<>();
-        int left = 0, best = 0;
+        // Approach 1 (HashMap)
+        // Map<Character, Integer> temp = new HashMap<>();
+        // int left = 0, result = 0;
+        // for (int right = 0; right < s.length(); right++) {
+        //     char ch = s.charAt(right);
+        //     if (temp.containsKey(ch) && temp.get(ch) >= left) {
+        //         left = temp.get(ch) + 1;
+        //     }
+        //     temp.put(ch, right);
+        //     result = Math.max(result, right - left + 1);
+        // }
+        // return result;
+
+        //Approach 2 (int Array of size 256(Extended ASCII)
+        int[] temp = new int[256];
+        Arrays.fill(temp, -1);
+        int left = 0, result = 0;
         for (int right = 0; right < s.length(); right++) {
             char ch = s.charAt(right);
-            if (last.containsKey(ch) && last.get(ch) >= left) {
-                left = last.get(ch) + 1;
+            if (ch < 256 && temp[ch] >= left) {
+                left = temp[ch] + 1;
             }
-            last.put(ch, right);
-            best = Math.max(best, right - left + 1);
+            if (ch < 256) temp[ch] = right;
+            result = Math.max(result, right - left + 1);
         }
-        return best;
+        return result;
     }
     public static void main(String[] args) {
         System.out.println(longestSubString("abcabcbb")); // 3 ("abc")
