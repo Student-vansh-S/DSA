209bca41bd8a42e4298b4d72d709d5072333471a
Vansh <sainivansh8116@gmail.com>
2026-02-10 00:01:38 +0530

    Single Element In A Sorted Array

--- Array/SingleElementInSortedArray.java
+++ Array/SingleElementInSortedArray.java
@@ -1,5 +1,28 @@
 public class SingleElementInSortedArray {
     public static int singleNonDuplicate(int[] nums) {
+        //Approach 1
+        // int n = arr.length;
+        // if(n==1) return arr[0];
+        // int st=0, end=n-1;
+        //     while(st<end){
+        //         int mid =st+(end-st)/2;
+
+        //         if(mid==0 && arr[0]!=arr[1]) return arr[0];
+        //         if(mid==n-1 &&arr[n-1]!=arr[n-2]) return arr[n-1];
+        //         if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]) return arr[mid];
+
+        //         if(mid%2==0){
+        //             if(arr[mid]==arr[mid-1]) end=mid+1;
+        //             else st=mid+1;
+        //         }
+        //         else{
+        //             if(arr[mid]==arr[mid-1]) st=mid+1;
+        //             else end=mid;
+        //         }             
+        //     }
+        //     return arr[st];
+
+        //Approach 2
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
