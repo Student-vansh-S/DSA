bb9a3a864ad5e470c56c3c772546a763861a8f74
Vansh <sainivansh8116@gmail.com>
2026-02-13 00:04:29 +0530

    Reverse Words In String

---
+++ String/ReverseWordsInString.java
@@ -0,0 +1,18 @@
+public class ReverseWordsInString {
+    public static String reverseWords(String s) {
+        s = s.trim();
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder(); 
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s="  Hello   , Welcome    back to coding !  ";
+        System.out.println(reverseWords(s));
+    }
+}
