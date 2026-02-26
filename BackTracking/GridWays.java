c4bf6d3dc0375d0a81df4de742975338fe8dcc95
Vansh Saini <sainivansh8116@gmail.com>
2026-02-26 23:44:05 +0530

    Add Leetcode solution for unique paths problem
    Added commented-out Leetcode solution for unique paths using permutation formula.

--- BackTracking/GridWays.java
+++ BackTracking/GridWays.java
@@ -15,6 +15,35 @@ public class GridWays {
         int NmM= fact((n-1 + m-1));
         int repeatChar= fact(n-1) * fact(m-1);
         return NmM / repeatChar;
+
+
+        // Leetcode Code
+        /*
+         * class Solution {
+         * public int uniquePaths(int m, int n) {
+         * return gridways(0, 0, m, n);
+         * }
+         * public int gridways(int i, int j, int m, int n) {
+         * // (Optional) Correct recursion version (very slow for big m,n)
+         * // if (i == m - 1 && j == n - 1) return 1;
+         * // if (i >= m || j >= n) return 0;
+         * // return gridways(i + 1, j, m, n) + gridways(i, j + 1, m, n);
+         * 
+         * // ✅ Permutation / Combination formula:
+         * // total moves = (m-1) downs + (n-1) rights = N
+         * // answer = C(N, r) where r = min(m-1, n-1)
+         * int N = (m - 1) + (n - 1);
+         * int r = Math.min(m - 1, n - 1);
+         * 
+         * long ans = 1;
+         * for (int k = 1; k <= r; k++) {
+         * ans = ans * (N - r + k) / k;
+         * }
+         * 
+         * return (int) ans;
+         * }
+         * }
+         */
     }
 
     public static int fact(int n){
