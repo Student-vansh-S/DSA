4c76c3ecc113f5f7db4a725b2e0719dfeefa86dd
Vansh <sainivansh8116@gmail.com>
2026-03-22 22:54:36 +0530

    Best Time To Buy And Sell Stocks

---
+++ Greedy Algorithm/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,21 @@
+public class BestTimeToBuyAndSellStocks {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = maxProfit(prices);
+        System.out.println(result);
+    }
+}
