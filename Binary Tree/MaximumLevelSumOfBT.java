3aaecf76d13ba44fa3fb3fbaf39cf0dd71d71db3
Vansh <sainivansh8116@gmail.com>
2026-03-25 23:49:21 +0530

    Maximum Level Sum Of Binary Tree

---
+++ Binary Tree/MaximumLevelSumOfBT.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class MaximumLevelSumOfBT {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int level = 1;
+        int answerLevel = 1;
+        int maxSum = Integer.MIN_VALUE;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int currentSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                currentSum += node.val;
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                answerLevel = level;
+            }
+
+            level++;
+        }
+
+        return answerLevel;
+    }
+}
