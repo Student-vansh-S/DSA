ae4600d030d55b1e8273618f4000303cc7911204
Vansh <sainivansh8116@gmail.com>
2026-04-04 21:46:31 +0530

    Range Sum Of BST

---
+++ BST/RangeSumOfBST.java
@@ -0,0 +1,30 @@
+public class RangeSumOfBST {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+
+        // If node value is smaller → skip left
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+
+        // If node value is greater → skip right
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+
+        // Node is in range
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}
