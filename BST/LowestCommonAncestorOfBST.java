e294c66f6becf61e18ab2cc6a4668c7a6190220c
Vansh <sainivansh8116@gmail.com>
2026-04-12 23:48:33 +0530

    Lowest Common Ancestor Of BST

---
+++ BST/LowestCommonAncestorOfBST.java
@@ -0,0 +1,28 @@
+public class LowestCommonAncestorOfBST {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root; // LCA found
+        }
+
+        return (left != null) ? left : right;
+    }
+}
