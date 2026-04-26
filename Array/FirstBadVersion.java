35b4e7f0fdb757b718bf897db9e26667bdbff777
Vansh <sainivansh8116@gmail.com>
2026-04-26 19:20:56 +0530

    First Bad Version

---
+++ Array/FirstBadVersion.java
@@ -0,0 +1,17 @@
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (isBadVersion(mid)) {
+                end = mid; // go left
+            } else {
+                start = mid + 1; // go right
+            }
+        }
+
+        return start;
+    }
+}
