f4820b696d2c88c8825231a4f4aa0cd2996ee498
Vansh <sainivansh8116@gmail.com>
2026-05-06 22:54:17 +0530

    Rotating The Box

---
+++ 2D Array/RotatingTheBox.java
@@ -0,0 +1,28 @@
+public class RotatingTheBox {
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        // Step 1: apply gravity
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    empty = j - 1;
+                } 
+                else if (box[i][j] == '#') {
+                    box[i][j] = '.';
+                    box[i][empty] = '#';
+                    empty--;
+                }
+            }
+        }
+        // Step 2: rotate matrix
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = box[i][j];
+            }
+        }
+        return res;
+    }
+}
