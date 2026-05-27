e8b48ef21644615d118de0e8f6140c6b6b9d9518
Vansh <sainivansh8116@gmail.com>
2026-05-27 23:17:37 +0530

    Count The Number Of Special Character II

---
+++ String/CountTheNumberOfSpecialCharacterII.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class CountTheNumberOfSpecialCharacterII {
+    public int numberOfSpecialChars(String word) {
+        int lowercase[]=new int[26];
+        int uppercase[]=new int[26];
+        Arrays.fill(lowercase,-1);
+        Arrays.fill(uppercase,-1);
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                lowercase[ch-'a']=i;
+            }
+            else{
+                int index= ch-'A';
+                if(uppercase[index]==-1)
+                {
+                    uppercase[index]=i;
+                }
+            }
+        }  
+        for(int i=0;i<26;i++)
+        {
+            if(lowercase[i]!=-1 && uppercase[i]!=-1 && lowercase[i]<uppercase[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
